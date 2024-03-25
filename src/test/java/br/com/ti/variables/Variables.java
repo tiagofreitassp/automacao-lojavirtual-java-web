c89e3649c0e4314c459b5b07ad44ed8f70e0d7a7
Tiago Freitas <tiago.freitas.sp@gmail.com>
2024-03-25 15:56:17 -0300

    Update Variables.java

--- src/test/java/br/com/ti/variables/Variables.java
+++ src/test/java/br/com/ti/variables/Variables.java
@@ -3,75 +3,74 @@ package br.com.ti.variables;
 public class Variables {
     public String var = "";
 
-    public String btnSignIn = "//a[@class='login']";
-    public String cpEmailAddress = "email_create";
-    public String btnCreateAnAccount = "//button[@id='SubmitCreate']";
-    public String rdTitleMr = "id_gender1";
-    public String rdTitleMrs = "id_gender2";
+    public String lblTotalProducts = "total_product";
+    public String lblTotalShipping = "total_shipping";
+    public String lblTotal = "total_price";
+    public String lblProductSuccessfullyAddedToYourShoppingCart_TotalProducts = "//strong[contains(text(),'Total products')]";
+    public String lblProductSuccessfullyAddedToYourShoppingCart_TotalShipping = "//strong[contains(text(),'Total shipping')]";
+    public String lblProductSuccessfullyAddedToYourShoppingCart_Total = "//strong[contains(text(),'Total')]";
+    public String lbWomen = "//a[@class='sf-with-ul'][contains(text(),'Women')]";
+    public String lbProduto1 = "//body/div[@id='page']/div[2]/div[1]/div[3]/div[2]/ul[1]/li[1]/div[1]/div[1]/div[1]/a[1]/img[1]\n";
+    public String lblShoppingCartSummary = "cart_title";
+    public String lbState = "//option[contains(text(),'Alaska')]";
+    public String lbDia = "//div[@id='uniform-days']//option[2]";
+    public String lbMes = "//option[contains(text(),'February')]";
+    public String lbAno = "//option[contains(text(),'2000')]";
+    public String lblTitle = "//label[contains(text(),'Title')]";
+
+    public String slAno = "//select[@id='years']";
+    public String slMes = "//select[@id='months']";
+    public String slDia = "//select[@id='days']";
+    public String slState = "//select[@id='id_state']";
+
     public String cpFirstName = "customer_firstname";
     public String cpLastName = "customer_lastname";
     public String cpEmail = "email";
     public String cpPassword = "passwd";
-    public String slDia = "//select[@id='days']";
-    public String lbDia = "//div[@id='uniform-days']//option[2]";
-    public String slMes = "//select[@id='months']";
-    public String lbMes = "//option[contains(text(),'February')]";
-    public String slAno = "//select[@id='years']";
-    public String lbAno = "//option[contains(text(),'2000')]";
-    public String lblTitle = "//label[contains(text(),'Title')]";
     public String cpYaFirstName = "firstname";
     public String cpYaLastName = "lastname";
     public String cpCompany = "company";
     public String cpAddress = "address1";
     public String cpCity = "city";
-    public String slState = "//select[@id='id_state']";
-    public String lbState = "//option[contains(text(),'Alaska')]";
     public String cpZip = "postcode";
     public String cpHomePhone = "phone";
     public String cpMobilePhone = "phone_mobile";
     public String cpAlias = "alias";
-    public String btnRegister = "//button[@id='submitAccount']";
-
-    //Welcome to your account. Here you can manage all of your personal information and orders.
-    public String txtBoasVindas = "//p[@class='info-account']";
-
     public String cpArEmailAddress = "email";
     public String cpArPassword = "passwd";
-    public String btnArSignin = "SubmitLogin";
-    public String lbWomen = "//a[@class='sf-with-ul'][contains(text(),'Women')]";
-    public String lbProduto1 = "//body/div[@id='page']/div[2]/div[1]/div[3]/div[2]/ul[1]/li[1]/div[1]/div[1]/div[1]/a[1]/img[1]\n";
-    public String btnAddToCart = "Submit";
-    public String txtProductSuccessfullyAddedToYourShoppingCart = "//*[contains(text(),'Product successfully added to your shopping cart')]";
-    public String txtProductSuccessfullyAddedToYourShoppingCart2 = "//td[@class='cart_description']//a[contains(text(),'Faded Short Sleeve T-shirts')]";
-    public String btnProceedToCheckout = "//a[@title='Proceed to checkout']";
-    public String btnContinueShopping = "//span[@title='Continue shopping']";
-    public String lblShoppingCartSummary = "cart_title";
-    public String btnSCSProceedToCheckout = "//a[@class='button btn btn-default standard-checkout button-medium']";
-    public String btnAdProceedToCheckout = "processAddress";
-    public String txtShipping = "//h1[@class='page-heading']";
     public String ckTermsOfService = "cgv";
-    public String btnShProceedToCheckout = "processCarrier";
-    public String txtPleaseChooseYourPaymentMethod = "//h1[@class='page-heading']";
-    public String lblTotalProducts = "total_product";
-    public String lblTotalShipping = "total_shipping";
-    public String lblTotal = "total_price";
-    public String btnPayByBankWire = "//div[@class='paiement_block']//div[1]//div[1]//p[1]";
-    public String txtOrderSummary = "//h1[@class='page-heading']";
-    public String txtBankWirePayment = "//h3[@class='page-subheading']";
-    public String btnIconfirmMyOrder = "//button[@class='button btn btn-default button-medium']";
+    public String cpEmailAddress = "email_create";
+
     public String txtOrderConfirmation = "//h1[@class='page-heading']";
     public String txtYourOrderOnMyStoreIsComplete = "//strong[contains(text(),'Your order on My Store is complete.')]";
     public String txtProduto_PrintedChiffonDress = "//body/div[@id='page']/div[2]/div[1]/div[3]/div[2]/ul[1]/li[7]/div[1]/div[1]/div[1]/a[1]/img[1]";
     public String txtProduto_Blouse =  "//body/div[@id='page']/div[2]/div[1]/div[3]/div[2]/ul[1]/li[2]/div[1]/div[1]/div[1]/a[1]/img[1]";
     public String txtFadedShortSleeveTshirts = "//h1[contains(text(),'Faded Short Sleeve T-shirts')]";
     public String txtPrintedChiffonDress = "//h1[contains(text(),'Printed Chiffon Dress')]";
+    public String txtOrderSummary = "//h1[@class='page-heading']";
+    public String txtBankWirePayment = "//h3[@class='page-subheading']";
+    public String txtPleaseChooseYourPaymentMethod = "//h1[@class='page-heading']";
+    public String txtShipping = "//h1[@class='page-heading']";
+    public String txtProductSuccessfullyAddedToYourShoppingCart = "//*[contains(text(),'Product successfully added to your shopping cart')]";
+    public String txtProductSuccessfullyAddedToYourShoppingCart2 = "//td[@class='cart_description']//a[contains(text(),'Faded Short Sleeve T-shirts')]";
+    public String txtBoasVindas = "//p[@class='info-account']";
 
-    public String lblProductSuccessfullyAddedToYourShoppingCart_TotalProducts = "//strong[contains(text(),'Total products')]";
-
-    public String lblProductSuccessfullyAddedToYourShoppingCart_TotalShipping = "//strong[contains(text(),'Total shipping')]";
-
-    public String lblProductSuccessfullyAddedToYourShoppingCart_Total = "//strong[contains(text(),'Total')]";
+    public String btnSignIn = "//a[@class='login']";
     public String btnSignOut = "//a[@class='logout']";
+    public String btnCreateAnAccount = "//button[@id='SubmitCreate']";
+    public String btnRegister = "//button[@id='submitAccount']";
+    public String btnArSignin = "SubmitLogin";
+    public String btnAddToCart = "Submit";
+    public String btnProceedToCheckout = "//a[@title='Proceed to checkout']";
+    public String btnContinueShopping = "//span[@title='Continue shopping']";
+    public String btnSCSProceedToCheckout = "//a[@class='button btn btn-default standard-checkout button-medium']";
+    public String btnAdProceedToCheckout = "processAddress";
+    public String btnShProceedToCheckout = "processCarrier";
+    public String btnPayByBankWire = "//div[@class='paiement_block']//div[1]//div[1]//p[1]";
+    public String btnIconfirmMyOrder = "//button[@class='button btn btn-default button-medium']";
+
+    public String rdTitleMr = "id_gender1";
+    public String rdTitleMrs = "id_gender2";
 
     public String iFrame_FadedShortSleeveTshirts = "//iframe[@class='fancybox-iframe']";
 
